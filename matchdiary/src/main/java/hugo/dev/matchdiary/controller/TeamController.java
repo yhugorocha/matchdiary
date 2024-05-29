4e5c78e0175f2377ec4df7da4f7d5cfd15a86937
Hugo Silva <yhugorocha@gmail.com>
2024-05-29 11:45:57 -0300

    fix: Correção para ao invez de retornar uma responseEntity<Team> agora retornar uma responseEntity<TeamDTO> nos metodos GET.

--- matchdiary/src/main/java/hugo/dev/matchdiary/controller/TeamController.java
+++ matchdiary/src/main/java/hugo/dev/matchdiary/controller/TeamController.java
@@ -1,18 +1,14 @@
 package hugo.dev.matchdiary.controller;
 
-import hugo.dev.matchdiary.model.Team;
+import hugo.dev.matchdiary.dto.team.TeamDTO;
 import hugo.dev.matchdiary.service.TeamService;
-import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/team")
@@ -22,24 +18,19 @@ public class TeamController {
     TeamService teamService;
 
     @GetMapping("/all")
-    public ResponseEntity<List<Team>> getAllteams(){
-        List<Team> listTeams = teamService.findAll();
-        return ResponseEntity.ok(listTeams);
+    public ResponseEntity<List<TeamDTO>> getAllteams(){
+        return ResponseEntity.ok(teamService.findAll());
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Team> getFindById(@PathVariable(value = "id") Long id){
-        Team team = teamService.findById(id).orElseThrow();
-
-        return new ResponseEntity<>(team, HttpStatus.CREATED);
+    public ResponseEntity<TeamDTO> getFindById(@PathVariable(value = "id") Long id){
+        return ResponseEntity.ok(teamService.findById(id));
     }
 
     @GetMapping("/pagination")
-    public ResponseEntity<Page<Team>> getPagination(@RequestParam(defaultValue = "0") int page,
+    public ResponseEntity<Page<TeamDTO>> getPagination(@RequestParam(defaultValue = "0") int page,
                                     @RequestParam(defaultValue = "5") int size){
-
-        Pageable pageable = PageRequest.of(page, size);
-        return ResponseEntity.ok(teamService.findAll(pageable));
+        return ResponseEntity.ok(teamService.findAll(page, size));
     }
 
 }
--- matchdiary/src/main/java/hugo/dev/matchdiary/service/TeamService.java
+++ matchdiary/src/main/java/hugo/dev/matchdiary/service/TeamService.java
@@ -1,14 +1,18 @@
 package hugo.dev.matchdiary.service;
 
+import hugo.dev.matchdiary.dto.team.TeamDTO;
+import hugo.dev.matchdiary.exception.NotFoundException;
 import hugo.dev.matchdiary.model.Team;
 import hugo.dev.matchdiary.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class TeamService {
@@ -16,15 +20,23 @@ public class TeamService {
     @Autowired
     TeamRepository teamRepository;
 
-    public List<Team> findAll() {
-        return teamRepository.findAll();
+    public List<TeamDTO> findAll() {
+        return teamRepository.findAll().stream().map(team -> new TeamDTO(team)).collect(Collectors.toList());
     }
 
-    public Optional<Team> findById(Long id) {
-        return teamRepository.findById(id);
+    public TeamDTO findById(Long id) {
+        Team team = teamRepository.findById(id).orElseThrow(() -> new NotFoundException("Team not found."));
+        TeamDTO teamDTO = new TeamDTO(team);
+        return teamDTO;
     }
 
-    public Page<Team> findAll(Pageable pageable) {
-        return teamRepository.findAll(pageable);
+    public Page<TeamDTO> findAll(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return convertToDtoPage(teamRepository.findAll(pageable));
+    }
+
+    public Page<TeamDTO> convertToDtoPage(Page<Team> pageTeam){
+        List<TeamDTO> listTeam = pageTeam.stream().map(team -> new TeamDTO(team)).collect(Collectors.toList());
+        return new PageImpl<>(listTeam, PageRequest.of(pageTeam.getNumber(), pageTeam.getSize()), pageTeam.getTotalElements());
     }
 }
