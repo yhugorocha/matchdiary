ac014b8508678b3c655e1c70217f257606a1c958
Hugo Silva <yhugorocha@gmail.com>
2024-05-30 13:56:59 -0300

    feat: add nova exception de regra de negocio.

--- matchdiary/src/main/java/hugo/dev/matchdiary/infra/RestExceptionHandler.java
+++ matchdiary/src/main/java/hugo/dev/matchdiary/infra/RestExceptionHandler.java
@@ -1,11 +1,14 @@
 package hugo.dev.matchdiary.infra;
 
+import hugo.dev.matchdiary.exception.BusinessRuleException;
 import hugo.dev.matchdiary.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -14,4 +17,10 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     private ResponseEntity<String> TeamNotFoundHandler(NotFoundException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
+
+    @ExceptionHandler(BusinessRuleException.class)
+    private ResponseEntity<String> BusinessRuleHandler(BusinessRuleException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
 }
