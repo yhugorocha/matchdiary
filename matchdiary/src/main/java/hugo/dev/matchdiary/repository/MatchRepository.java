d9d46be136bef2a01b4f35b4c370156158e42924
Hugo Silva <yhugorocha@gmail.com>
2024-05-30 14:02:06 -0300

    feat: add nova query para verificar se existe 2 times cadastrados na mesma data.

--- matchdiary/src/main/java/hugo/dev/matchdiary/repository/MatchRepository.java
+++ matchdiary/src/main/java/hugo/dev/matchdiary/repository/MatchRepository.java
@@ -1,9 +1,18 @@
 package hugo.dev.matchdiary.repository;
 
 import hugo.dev.matchdiary.model.Match;
+import hugo.dev.matchdiary.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.Optional;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
+
+    @Query("SELECT m FROM Match m WHERE (m.teamOne = :team_one AND m.teamTwo = :team_two OR m.teamOne = :team_two AND m.teamTwo = :team_one) AND m.date = :date")
+    Optional<Match> findByMatchForTeamOneTeamTwoDate(@Param("team_one") Team team_one, @Param("team_two") Team team_two, @Param("date") Date date);
 }
